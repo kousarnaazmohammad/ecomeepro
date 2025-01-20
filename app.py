f243d9138a1aa49551c6f5a63acbba9da929b268
kousarnaazmohammad <kousarnaazm@gmail.com>
2025-01-20 18:32:55 +0530

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from otp import genotp    #fun name,var name,class name---->we call them as iden
 from cmail import sendmail
 from token_1 import encode,decode
 import os
-import razorpay
+#import razorpay
 import re
 import mysql.connector 
 from mysql.connector import (connection) 
@@ -11,7 +11,7 @@ from mysql.connector import (connection)
 app=Flask(__name__)
 app.secret_key="kousar@1" #to create flash msgs we create secret_key
 app.config['SESSION_TYPE']='filesystem'
-client = razorpay.Client(auth=("rzp_test_hO4xe5Tf68Z20c", "dHl9OlOpcvK75y0kLerSIMwI"))
+#client = razorpay.Client(auth=("rzp_test_hO4xe5Tf68Z20c", "dHl9OlOpcvK75y0kLerSIMwI"))
 user=os.environ.get('RDS_USERNAME')#aws keys
 db=os.environ.get('RDS_DB_NAME')
 password=os.environ.get('RDS_PASSWORD')
@@ -526,7 +526,7 @@ def description(itemid):
         flash("couldn't fetch items")
         return redirect(url_for('index'))
     return render_template('description.html',item_data=item_data)
-@app.route('/payment/<itemid>/<name>/<float:price>',methods=["GET","POST"])
+''''@app.route('/payment/<itemid>/<name>/<float:price>',methods=["GET","POST"])
 def payment(itemid,name,price):
     try:
         qyt=int(request.form.get('qyt'))
@@ -588,7 +588,7 @@ def orders():
         else:
             return render_template('orders.html',ordlist=ordlist)
     else:
-        return redirect(url_for('userlogin'))
+        return redirect(url_for('userlogin'))'''
 @app.route('/search',methods=["GET","POST"])
 def search():
         if request.method=="POST":
